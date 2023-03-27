5e7d0b23c015a9f1cf08faf1380744fd10cf5eb4
Conner <110003454+ConnerConnerConner@users.noreply.github.com>
2023-03-27 12:14:00 -0500

    Version 1.0.0-rc1

--- src/main.py
+++ src/main.py
@@ -1,19 +1,21 @@
 from json import loads as json_loads
+from re import L
 from pyperclip import copy
 import PySimpleGUI as sg
 from decimal import Decimal as d
-from math import floor as round_down
 
+#GUI Theme
 sg.theme('DarkGrey12')
 
-class timer:
+class retime: #Class for all timer related functions
     def frame_round(time, fps): # Rounds to the nearest frame
         time = d(time)
         time = round(time, 3)
         output = d(time - time%(d(1)/fps)) #Credit to Slush0Puppy for this 1 Line of Code
         return round(output, 3)
 
-    def format(time): # Formats the time to the SRC format
+    # Formats the time to the SRC format
+    def format(time):
         time = str(time)
         time = time.split('.', 1)
         seconds = time[0]
@@ -26,9 +28,11 @@ class timer:
             seconds = seconds - (minutes * 60)
         if minutes > 60: #makes sure that the minutes are less than 60
             minutes = minutes - (hours * 60)
+        #Converts Integers to Strings
         seconds = str(seconds)
         minutes = str(minutes)
         hours = str(hours)
+        #Combines the time into a single string
         if seconds == '0':
             return (f'0s {milliseconds}ms')
         elif minutes == '0':
@@ -41,76 +45,82 @@ class timer:
         else:
             return (f'{hours}h {minutes}m {seconds}s {milliseconds}ms')
         
-
-    def load(dbi_end, dbi_start, fps):
-        try:
+    #Calculates the loads
+    def loads(dbi_start, dbi_end, fps):
+        try: #Turns JSON into a Dictionary and Checks if it is Valid (Start)
             dbis_dict = json_loads(dbi_start)
         except:
-            sg.popup('Error (Start)', 'Debug Info is not valid.', title = 'Error')
+            sg.popup('Error (Start)', 'Debug Info is not valid.', title = 'Error') #Error Message
             return
-        try:
+        try: #Turns JSON into a Dictionary and Checks if it is Valid (End)
             dbie_dict = json_loads(dbi_end)
         except:
-            sg.popup('Error (End)', 'Debug Info is not valid.', title = 'Error')
+            sg.popup('Error (End)', 'Debug Info is not valid.', title = 'Error') #Error Message
             return
-        try:
+        try: #Gets the CMT from the Dictionary
             cmt_end = dbie_dict['cmt']
             cmt_start = dbis_dict['cmt']
         except:
-            sg.popup('Error (CMT)', 'CMT is not Valid.', title = 'fuck you rekto')
-        cmt_start = timer.frame_round(d(cmt_start), fps)
-        cmt_end = timer.frame_round(d(cmt_end), fps)
-        try:
-            time = (d(cmt_end) - d(cmt_start)) + d(time)
-        except:
-            sg.popup('Error (CMT)', 'CMT is not Valid.', title = 'fuck you rekto')
+            sg.popup('Error (CMT)', 'CMT is not Valid.', title = 'Error') #Error Message
+        #Rounds the CMT to the nearest frame
+        cmt_start = retime.frame_round(d(cmt_start), fps)
+        cmt_end = retime.frame_round(d(cmt_end), fps)
+        #Calculates the Loads
+        loads = (d(cmt_end) - d(cmt_start))
+        if -abs(loads) == loads: #Checks if the Start is greater than the End
+            sg.popup('Error', 'The start is greater than the end.', title = 'Error') #Error Message
             return
-        if -abs(time) == time:
-            sg.popup('Error', 'The start is greater than the end.', title = 'Error')
-            return
-        main_window['dbis_loads'].update('')
-        main_window['dbie_loads'].update('')
-        sg.popup(f'Loads Added', title = 'Loads', font = ('Helvetica', 16))
+        loads = retime.frame_round(loads, fps) #Rounds the Loads to the nearest frame just in case the rounding is off
+        sg.popup(f'Loads Added', title = 'Loads', font = ('Helvetica', 16)) #Success Message
         return loads
 
+    #Calculates the Final Time
     def final(dbi_start, dbi_end, loads, fps):
-        try:
+        try: #Turns JSON into a Dictionary and Checks if it is Valid (Start)
             dbis_dict = json_loads(dbi_start)
         except:
-            sg.popup('Error (Start)', 'Debug Info is not valid.', title = 'Error')
+            sg.popup('Error (Start)', 'Debug Info is not valid.', title = 'Error') #Error Message
             return
-        try:
+        try: #Turns JSON into a Dictionary and Checks if it is Valid (End)
             dbie_dict = json_loads(dbi_end)
         except:
-            sg.popup('Error (End)', 'Debug Info is not valid.', title = 'Error')
+            sg.popup('Error (End)', 'Debug Info is not valid.', title = 'Error') #Error Message
             return
-        try:
+        try: #Gets the CMT from the Dictionary
             cmt_end = dbie_dict['cmt']
             cmt_start = dbis_dict['cmt']
         except:
-            sg.popup('Error (CMT)', 'CMT is not Valid.', title = 'fuck you rekto')
-        cmt_start = timer.frame_round(d(cmt_start), fps)
-        cmt_end = timer.frame_round(d(cmt_end), fps)
+            sg.popup('Error (CMT)', 'CMT is not Valid.', title = 'Error') #Error Message
+        #Rounds the CMT to the nearest frame
+        cmt_start = retime.frame_round(d(cmt_start), fps)
+        cmt_end = retime.frame_round(d(cmt_end), fps)
+        #Calculates the Final Time
         time_loads = (d(cmt_end) - d(cmt_start))
-        if -abs(time_loads) == time_loads:
-            sg.popup('Error', 'The start is greater than the end.', title = 'Error')
+        if -abs(time_loads) == time_loads: #Checks if the Start is greater than the End
+            sg.popup('Error', 'The start is greater than the end.', title = 'Error') #Error Message
             return
-        loads = timer.frame_round(loads, fps)
-        time_noloads = time_loads - loads
-        no_loads = timer.format(time_noloads)
-        with_loads = timer.format(time_loads)
+        if loads > time_loads: #Checks if the Loads are greater than the Time
+            sg.popup('Error', 'The Loads is greater than the Time.', title = 'Error') #Error Message
+            return
+        loads = retime.frame_round(loads, fps) #Rounds Loads for the millionth time
+        time_noloads = time_loads - loads #Gets the Time without Loads
+        #Formats the Time
+        no_loads = retime.format(time_noloads)
+        with_loads = retime.format(time_loads)
         if loads == 0:
             final_confirm = sg.popup_yes_no(f'Without Loads: {no_loads}', 'Would you like the Mod Note to be Copied to the Clipboard?', title = 'Results')
             if final_confirm == 'Yes':
-                copy(f'Mod Note: Retimed to {no_loads} https://github.com/ConnerConnerConner/PyTime')
+                copy(f'Mod Note: Retimed to {no_loads} at {fps} using https://github.com/ConnerConnerConner/PyTime')
             elif final_confirm == 'No':
                return 
         else:
             final_confirm = sg.popup_yes_no(f'Without Loads: {no_loads}, With Loads: {with_loads}', 'Mod Note Copied to Clipboard', title = 'Results')
         if final_confirm == 'Yes':
-            copy(f'Mod Note: Retimed to {no_loads} using https://github.com/ConnerConnerConner/PyTime')
+            copy(f'Mod Note: Retimed to {no_loads} at {fps} using https://github.com/ConnerConnerConner/PyTime')
         elif final_confirm == 'No':
              return
+
+#GUI Layout
 main_layout = [
         [sg.Text('PyTime', font = ('Helvetica', 36)), sg.Text('   FPS', font = (' Helvetica', 30)), sg.InputText('60', size = (5, 1), key = 'fps', font = ('Helvetica', 30))],
         [sg.InputText(key = 'dbis', font = ('Helvetica', 16), pad = ((5, 0), (0, 0)), size = (20, 1)), sg.Text('  Debug Info Start', font = ('Helvetica', 16), justification = 'right')],
@@ -119,52 +129,61 @@ main_layout = [
         [sg.InputText(key = 'dbie_loads', font = ('Helvetica', 14), pad = ((5, 0), (0, 0)), size = (15, 1)), sg.Text('   Debug Info End (Loads)', font = ('Helvetica', 14), justification = 'right')],
         [sg.Button('Calculate', font = ('Helvetica', 16)), sg.Button('Add Loads', font = ('Helvetica', 16)), sg.Button('Remove All Loads', font = ('Helvetica', 16))]
     ]
-main_window = sg.Window('PyTime', main_layout, resizable = False, element_justification = 'left', size=(447, 253),  icon=r'assets\PyTime.ico')
+main_window = sg.Window('PyTime', main_layout, resizable = False, element_justification = 'left', size=(447, 253))
 
+#Main Loop
 while True:
-    event, values = main_window.read()
-    if event == sg.WIN_CLOSED:
+    event, values = main_window.read() #Reads the Window
+    if event == sg.WIN_CLOSED: #Checks if the Window is Closed
         break
-    if event == 'Remove All Loads':
-        lr_confirm = sg.popup_yes_no('Are you sure you want to remove all loads?', title = 'Remove All Loads', font = ('Helvetica', 16))
+    if event == 'Remove All Loads': #Checks if the Remove All Loads Button is Pressed
+        lr_confirm = sg.popup_yes_no('Are you sure you want to remove all loads?', title = 'Remove All Loads', font = ('Helvetica', 16), icon = r'assets\PyTime.ico') #Confirmation Message
         if lr_confirm == 'Yes':
+            #Clears Loads Input Boxes
             main_window['dbis_loads'].update('')
             main_window['dbie_loads'].update('')
-            loads = 0
+            loads = 0 #Sets Loads to 0
         elif lr_confirm == 'No':
             continue
-    if event == 'Add Loads':
+    if event == 'Add Loads': #Checks if the Add Loads Button is Pressed
+        #Gets the Values from the Input Boxes
         dbis_loads = values['dbis_loads']
         dbiel_loads = values['dbie_loads']
         fps = values['fps']
-        try:
+        try: #Checks if the FPS is Valid
             fps = d(fps)
         except:
-            sg.popup('Error (FPS)', 'FPS is not a valid number.', title = 'Error')
+            sg.popup('Error (FPS)', 'FPS is not a valid number.', title = 'Error') #Error Message
             continue
-        if not 'loads' in globals():
-            loads = timer.load(dbis_loads, dbiel_loads, fps)
+        if not 'loads' in globals(): #Checks if Loads exists
+            loads = retime.loads(dbis_loads, dbiel_loads, fps) #Calculates Loads
         else:
             try:
-                loads = timer.load(dbis_loads, dbiel_loads, fps) + loads
+                loads = retime.loads(dbis_loads, dbiel_loads, fps) + loads #Calculates Loads
             except:
                 continue
     if event == 'Calculate':
+        #Gets the Values from the Input Boxes
         dbi_start = values['dbis']
         dbi_end = values['dbie']
         fps = values['fps']
-        try:
+        try: #Checks if the FPS is Valid
             fps = d(fps)
         except:
             sg.popup('Error (FPS)', 'FPS is not an valid number.', title = 'Error')
             continue
-        if not 'loads' in globals():
-            loads = 0
-        main_window['dbis'].update('')
-        main_window['dbie'].update('')
-        timer.final(dbi_start, dbi_end, loads, fps)
+        if fps == 0:
+            sg.popup('Error (FPS)', 'FPS cannot be 0.', title = 'Error')
+            continue
+        else:
+            if not 'loads' in globals(): #Check if the Loads Variables Exists
+                loads = 0 #Sets Loads to 0
+            retime.final(dbi_start, dbi_end, loads, fps) #Runs the Final Function
+            #Clears Input Boxes
+            main_window['dbis'].update('')
+            main_window['dbie'].update('')
 
-main_window.close()
+main_window.close() #Closes the Window once
 
 #Credit to Rekto for Helping Me With Everything
 #Credit to Slush0Puppy for Frane Rounding
